54c8ef993df58ced93485848c455895d6e4fa549
Bakel Bakel <begededum4bakel@gmail.com>
2025-02-24 22:01:17 +0100

    creating the repo

--- net/testing_arduino_rasp/testing_arduino_rasp.ino
+++
@@ -1,8 +0,0 @@
-void setup() {
-    Serial.begin(9600);  // Start serial communication
-}
-
-void loop() {
-    Serial.println("Hello from Arduino!");  // Send test message
-    delay(1000);  // Send every 1 second
-}
