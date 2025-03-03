ade8b074c9b565a8e9af05200d9ca08b2316241d
Bakel Bakel <begededum4bakel@gmail.com>
2025-03-03 22:07:11 +0100

    pushing ex 1&2

---
+++ assessment_1.ino
@@ -0,0 +1,16 @@
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(9600);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  Serial.println("Hola, its LordCruise H.S Name Bakel");
+  digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on 
+  delay(1000);                      // wait for a second 1000 ms
+  digitalWrite(LED_BUILTIN, LOW);   // turn the LED off 
+  delay(1000);                      // wait for a second 1000 ms
+}
---
+++ assessment_2.ino
@@ -0,0 +1,25 @@
+byte receivedByte = 0;
+// the setup function runs once when you press reset or power the board
+void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(9600);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  if (Serial.available() > 0) //asking if character coming through serial port
+  {
+    //If there is a character, read it
+    receivedByte = Serial.read();
+    if (receivedByte == 79) // ascii: O
+    {
+      digitalWrite(LED_BUILTIN, LOW);
+    }
+    if (receivedByte == 82) // ascii: R
+    {
+      digitalWrite(LED_BUILTIN, HIGH);
+    }
+    Serial.println(receivedByte); //send character back
+  }
+}
